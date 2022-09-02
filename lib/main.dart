bebfa7213fd2e18794d6fc2a955d239c5e1985d0
RezaMahdinezhad <RezaMahdinezhad@gmail.com>
2022-09-02 16:42:48 +0430

    final v

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,61 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Application());
+  runApp(GameApplication());
 }
 
-class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
+class GameApplication extends StatefulWidget {
+  GameApplication({Key? key}) : super(key: key);
 
+  @override
+  State<GameApplication> createState() => _GameApplicationState();
+}
+
+class _GameApplicationState extends State<GameApplication> {
+  var top = 1;
+  var bottom = 2;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'vb'),
       home: Scaffold(
-        backgroundColor: Colors.deepOrange,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          backgroundColor: Colors.red,
-          elevation: 0.0,
+          backgroundColor: Colors.blue,
+          elevation: 4.0,
           centerTitle: true,
           title: Text('سنگ   کاغذ   قیچی'),
         ),
         body: SafeArea(
-          child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image(
-                  image: AssetImage('images/2.png'),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'شروع بازی',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                Image(
-                  image: AssetImage('images/3.png'),
-                ),
-              ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image(
+              image: AssetImage('images/$top.png'),
+              height: 100,
             ),
-          ),
-        ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  top = Random().nextInt(3) + 1;
+                  bottom = Random().nextInt(3) + 1;
+                });
+              },
+              child: Text(
+                'شروع بازی',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Image(
+              image: AssetImage('images/$bottom.png'),
+              height: 100,
+            ),
+          ],
+        )),
       ),
     );
   }
